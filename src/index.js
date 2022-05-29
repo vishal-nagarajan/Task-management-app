10d6911644a597f7a5ee071a294aafb93e31b5aa
vishal-nagarajan <vishal.n@peoplehum.com>
2022-05-29 20:42:55 +0530

    Adding project to git

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Register from './view/Register';
+import TentativeTodo from './view/TentativeToDo'
+import SignIn from './view/SignIn';
+import TaskForTheDay from './view/TaskForTheDay';
+import Tasks from './view/Tasks';
+import AddTask from './view/AddTask';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import NavBar from './view/NavBar';
+import Landing from './view/Landing';
+import UpdateWork from './view/UpdateWork';
+import CompletedTasks from './view/TaskListViews/CompletedTasks';
+import WorkLog from './view/WorkLog';
+import EditTask from './view/EditTask';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <div className="app-container">
+        {/* <NavBar/> */}
+        <Switch>
+        <Route path="/homepage" component={Landing} />
+          <Route path="/signup" component={Register} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/tentative-tasks" component={TentativeTodo} />
+          <Route path="/task-for-today" component={TaskForTheDay} />
+          <Route path="/task/work/:taskId" component={UpdateWork} />
+          <Route path="/task/completed" component={CompletedTasks} />
+          <Route path='/task/task-in-progress' component={CompletedTasks} />
+          <Route path='/task/task-over-due' component={CompletedTasks} />
+          <Route path='/task/all' component={CompletedTasks} />
+          <Route path='/task/task-yet-to-be-due' component={CompletedTasks} />
+          <Route path='/task/work-log' component={WorkLog} />
+          <Route path='/task/edit/:taskId' component={EditTask} />
+        </Switch>
+      </div>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
